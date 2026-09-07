3d33243a89524c5ab0e5f22642b586a7936ee922
Tarun Panchal <tarunpanchal047@gmail.com>
2026-09-07 11:31:00 +0530

    Updated project

--- 01-React-Project/src/useEffect/UseEffect2.jsx
+++ 01-React-Project/src/useEffect/UseEffect2.jsx
@@ -10,6 +10,7 @@ const UseEffect2 = () => {
   return (
     <div>
       <h1>useEffect2 : {count}</h1>
+      <h1>useEffect2 : {count}</h1>
       <button onClick={()=>{setCount(count++)}}>Increment</button>
       <button onClick={()=>{setCount(count--)}}>Decrement</button>
       <button onClick={()=>{setCount(0)}}>Reset</button>
