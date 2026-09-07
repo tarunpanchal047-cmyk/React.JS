3db8b1b114e95d3e8adcf45209036b7048360ba9
Tarun Panchal <tarunpanchal047@gmail.com>
2026-09-07 11:27:40 +0530

    Updated project

--- 01-React-Project/src/useEffect/UseEffect2.jsx
+++ 01-React-Project/src/useEffect/UseEffect2.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import  { useEffect, useState } from 'react'
 
 const UseEffect2 = () => {
- let [count, setCount] = useState(0)
-useEffect(()=>{
-   document.title = `Message${count}`
-},[])
 
+    let [count, setCount] = useState(0)
+    useEffect(() => {
+     document.title = `Message${count}`
+    }, [count])
+    
   return (
     <div>
-        <h1>useEffect2 : {count}</h1>
-        <button onClick={()=>{setCount(count++)}}>Increment</button>
-      
+      <h1>useEffect2 : {count}</h1>
+      <button onClick={()=>{setCount(count++)}}>Increment</button>
+      <button onClick={()=>{setCount(count--)}}>Decrement</button>
+      <button onClick={()=>{setCount(0)}}>Reset</button>
     </div>
   )
 }
 
 export default UseEffect2
+
